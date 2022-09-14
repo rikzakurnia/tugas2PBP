c60d08160d6223e354b8b3ce6a2969899bfc6078
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-09-14 18:22:59 +0700

    finish setting MTV and migration

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from django.shortcuts import render
+from django.shortcuts import render
+from katalog.models import CatalogItem
+
+# Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Rikza Kurnia',
+    'ID' : '2106701293'
+    }
+    return render(request, "katalog.html", context)
