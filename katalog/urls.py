41551a771f9612a035d93df3cf0d4685cc0f4517
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-09-14 18:37:59 +0700

    fix app name in urls.py

--- katalog/urls.py
+++ katalog/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from katalog.views import show_katalog
 
-app_name = 'wishlist'
+app_name = 'katalog'
 
 urlpatterns = [
     path('', show_katalog, name='show_katalog'),
