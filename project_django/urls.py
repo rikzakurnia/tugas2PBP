e5642c6fc6dafd2ac83f4beb590e36c5135047d2
rikzakurnia <112408954+rikzakurnia@users.noreply.github.com>
2022-09-14 19:08:29 +0700

    adding empty urlpattern

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('katalog.urls')),
+    path('katalog/', include('katalog.urls')),
+    path('', include('example_app.urls')),
 ]
 
 # TODO: Implement Routings Here
