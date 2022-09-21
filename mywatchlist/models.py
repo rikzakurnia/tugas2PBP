e88aea3abb007644c1c079e889269b3d1b26b27f
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-09-21 20:19:26 +0700

    add database

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,3 +1,11 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class MovieWatchList(models.Model):
+    status_watched = models.BooleanField(default=False)
+    judul = models.CharField(max_length=50)
+    rating = models.FloatField()
+    release_date = models.DateField()
+    review = models.URLField()
+    
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+# TODO: Create your views here.
+from mywatchlist.models import MovieWatchList
+
 # Create your views here.
+def show_watchlist(request):
+    judul_movie_watchlist = MovieWatchList.objects.all()
+    context = {
+    'judul_movie': judul_movie_watchlist,
+    'nama': 'Rikza Kurnia Almujtaba Lubis',
+    'ID' : '2106701293'
+    }
+    return render(request, "mywatchlist.html", context)
