e0e17301756e52b0f7abab2d6538fc4b9d8d6d44
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-09-22 06:31:15 +0700

    routing and fix links

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 
 # TODO: Create your views here.
 from mywatchlist.models import MovieWatchList
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_watchlist(request):
-    judul_movie_watchlist = MovieWatchList.objects.all()
+    list_movie_watchlist = MovieWatchList.objects.all()
     context = {
-    'judul_movie': judul_movie_watchlist,
+    'list_movie': list_movie_watchlist,
     'nama': 'Rikza Kurnia Almujtaba Lubis',
     'ID' : '2106701293'
     }
     return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    list_movie_watchlist = MovieWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", list_movie_watchlist), content_type="application/xml")
+
+def show_json(request):
+    list_movie_watchlist = MovieWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", list_movie_watchlist), content_type="application/json")
