b54c2adf692e687915cb2db3a64332677f2adf60
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-09-22 06:50:42 +0700

    add bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -8,10 +8,20 @@ from django.core import serializers
 # Create your views here.
 def show_watchlist(request):
     list_movie_watchlist = MovieWatchList.objects.all()
+    jumlah_movie_ditonton = 0
+    for movie in list_movie_watchlist:
+        if movie.status_watched:
+            jumlah_movie_ditonton += 1
+    
+    banyak_nonton = False
+    if jumlah_movie_ditonton>(len(list_movie_watchlist)/2):
+        banyak_nonton = True
+
     context = {
     'list_movie': list_movie_watchlist,
     'nama': 'Rikza Kurnia Almujtaba Lubis',
-    'ID' : '2106701293'
+    'ID' : '2106701293',
+    'banyak_nonton' : banyak_nonton,
     }
     return render(request, "mywatchlist.html", context)
 
