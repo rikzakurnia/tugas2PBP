6e57543f1333cc3919c69039201a5aa8f572213a
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-09-22 09:02:40 +0700

    fix test.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,24 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
-from multiprocessing.connection import Client
-from urllib import response
-
-
-# Create your tests here.
 
 class MywatchlistTest(TestCase):
     def test_request_html(self):
-        # c = Client()
-        response = self.client.get('/mywatchlist/html/')
+        response = self.client.get('mywatchlist/html/')
         self.assertEqual(response.status_code,200)
 
     def test_request_xml(self):
-        # c = Client()
-        response = self.client.get('/mywatchlist/xml/')
+        response = self.client.get('mywatchlist/xml/')
         self.assertEqual(response.status_code,200)
 
     def test_request_json(self):
-        # c = Client()
-        response = self.client.get('/mywatchlist/json/')
+        response = self.client.get('mywatchlist/json/')
         self.assertEqual(response.status_code,200)
