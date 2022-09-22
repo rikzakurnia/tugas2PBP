bee8c74d4b69e4e2d3a1ca72ab4232ff416bb481
Rikza Kurnia <rikza.kurnia@ui.ac.id>
2022-09-22 08:21:51 +0700

    update models field on judul

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class MovieWatchList(models.Model):
     status_watched = models.BooleanField(default=False)
-    judul = models.CharField(max_length=50)
+    judul = models.TextField(max_length=100)
     rating = models.FloatField()
     release_date = models.DateField()
     review = models.URLField()
--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+from multiprocessing.connection import Client
+from urllib import response
+
+
+# Create your tests here.
+
+class MywatchlistTest(TestCase):
+    def test_request_html(self):
+        # c = Client()
+        response = self.client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_request_xml(self):
+        # c = Client()
+        response = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+    def test_request_json(self):
+        # c = Client()
+        response = self.client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
